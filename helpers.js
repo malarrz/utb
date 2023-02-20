a4db6bee8cbd9b438f9313eae0ef2cb52acb5daf
Malarrz <marco@malarrz.com>
2023-02-20 19:29:59 -0400

    GRAVATAR

--- helpers.js
+++ helpers.js
@@ -18,7 +18,7 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-// exports.siteName = `SISTEMA DE TIENDAS`;
+exports.siteName = `SISTEMA DE TIENDAS`;
 
 exports.menu = [
   { slug: '/tiendas', title: 'Tiendas', icon: 'store', },
