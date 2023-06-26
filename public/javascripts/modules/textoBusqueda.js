ebb6caea204dde84548971feaf7515f6d58dd16e
Malarrz <marco@malarrz.com>
2023-06-25 21:55:11 -0400

    busqueda

--- public/javascripts/modules/textoBusqueda.js
+++ public/javascripts/modules/textoBusqueda.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+import axios from 'axios';
+import dompurify from 'dompurify';
 
 function resultadosBusquedaHTML(tiendas) {
     return tiendas.map(tienda => {
@@ -28,10 +29,41 @@ function textoBusqueda(search) {
             .get(`/api/search?q=${this.value}`)
             .then(res => {
                 if (res.data.length) {
-                    busquedaResultados.innerHTML = resultadosBusquedaHTML(res.data);
+                    busquedaResultados.innerHTML = dompurify.sanitize(resultadosBusquedaHTML(res.data));
+                    return;
                 }
+                busquedaResultados.innerHTML = dompurify.sanitize(`<div class="search__result">No hay resultados de la búsqueda '${this.value}'</div>`);
+            })
+            .catch(err => {
+                console.error(err);
             });
     });
-};
+    //Navegación de la búsqueda con teclado arriba, abajo, enter
+    busquedaTexto.on('keyup', (e) => {
+        if (![38, 40, 13].includes(e.keyCode)) {
+          return;
+        }
+        const activeClass = 'search__result--active';
+        const current = search.querySelector(`.${activeClass}`);
+        const items = search.querySelectorAll('.search__result');
+        let next;
+        if (e.keyCode === 40 && current) {
+          next = current.nextElementSibling || items[0];
+        } else if (e.keyCode === 40) {
+          next = items[0];
+        } else if (e.keyCode === 38 && current) {
+          next = current.previousElementSibling || items[items.length - 1]
+        } else if (e.keyCode === 38) {
+          next = items[items.length - 1];
+        } else if (e.keyCode === 13 && current.href) {
+          window.location = current.href;
+          return;
+        }
+        if (current) {
+          current.classList.remove(activeClass);
+        }
+        next.classList.add(activeClass);
+      });
+    }
 
 export default textoBusqueda;
